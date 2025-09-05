c57bb4c35f10f41deb2f255ecea781397a81aabb
Vasil Ivanov <ivanovvasil99@gmail.com>
2025-09-06 00:16:38 +0300

    Add stronger attraction version

--- libs/rhs.py
+++ libs/rhs.py
@@ -24,13 +24,26 @@ def MM2_a50_r1_jax(a, b, c):
     return f
 
 
+@jax.jit
+def MM2_a200_r1_jax(a, b, c):
+    f = (
+        -200 * ((-a + b) ** -1)
+        + (-b + c) ** -1
+        + 1 * ((-a + b) ** -3 - ((-b + c) ** -3))
+    )
+    return f
+
+
 class RHSType(enum.StrEnum):
     MM2_a50_r1 = "MM2_a50_r1"
     MM2_a50_r1_jax = "MM2_a50_r1_jax"
+    MM2_a200_r1_jax = "MM2_a200_r1_jax"
 
     def into_rhs(self):
         if self == RHSType.MM2_a50_r1:
             return MM2_a50_r1
         elif self == RHSType.MM2_a50_r1_jax:
             return MM2_a50_r1_jax
+        elif self == RHSType.MM2_a200_r1_jax:
+            return MM2_a200_r1_jax
         raise ValueError(f"Unknown RHS type: {self}")
